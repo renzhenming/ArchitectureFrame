809845f72d0bf19c4e5c610846c826cc794fdd50
renzhenming <1565766177@qq.com>
2018-06-17 10:51:46 +0800

    add

--- app/src/main/java/com/app/rzm/test/TestSimpleImageLoaderActivity.java
+++ app/src/main/java/com/app/rzm/test/TestSimpleImageLoaderActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.app.rzm.R;
 import com.app.rzm.ui.selectimage.ChoosePictureActivity;
+import com.rzm.commonlibrary.general.imageloader.loader.SimpleImageLoader;
 import com.rzm.commonlibrary.general.permission.PermissionDenied;
 import com.rzm.commonlibrary.general.permission.PermissionHelper;
 import com.rzm.commonlibrary.general.permission.PermissionSucceed;
@@ -19,6 +21,8 @@ public class TestSimpleImageLoaderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_simple_image_loader);
+        ImageView image = (ImageView) findViewById(R.id.imageview);
+        SimpleImageLoader.getInstance().display(image,"https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3434597041,1190385826&fm=27&gp=0.jpg");
     }
 
     public void jump(View view) {
